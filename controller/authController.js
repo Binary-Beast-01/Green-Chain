394ec10c3e9498efe5053546ba0aa5720004786e
MUKILAN S <mukilan069@gmail.com>
2023-02-18 23:44:31 +0530

    Authentication Done

--- controller/authController.js
+++ controller/authController.js
@@ -1,3 +1,13 @@
+const sqlite3 = require("sqlite3").verbose();
+
+const db = new sqlite3.Database("./database/database.db", (err) => {
+    if (err) {
+        return console.error(err.message);
+    } else {
+        //console.log("Database Connected");
+    }
+});
+
 const Login = async (req, res) => {
     res.render('login')
   }
@@ -6,12 +16,25 @@ const Login = async (req, res) => {
   }
 
   const PostLogin = async (req, res) => {
-    res.render('login')
+    db.get('SELECT * FROM user WHERE wallet = ?',[req.body.wallet_id],(err,result)=>{
+        if(result){
+            res.cookie("walletID", req.body.wallet_id);
+            res.cookie("role", result.role);
+            res.cookie("name", result.name);
+            res.json("OK")
+        }else{
+            res.redirect('/auth/register')
+        }
+    })
   }
   const PostRegister = async (req, res) => {
-    res.render('register')
+    db.run('INSERT INTO user(name,role,mail,wallet) VALUES(?,?,?,?)',[req.body.name,req.body.role,req.body.mail,req.body.wallet_id],(err)=>{
+        res.json("OK")
+    })
   }
   module.exports = {
     Login,
-    Register
+    Register,
+    PostLogin,
+    PostRegister
   }
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -8,5 +8,8 @@ const router = express.Router();
 router.get("/login", AuthController.Login)
 router.get("/register", AuthController.Register)
 
+router.post("/login", AuthController.PostLogin)
+router.post("/register", AuthController.PostRegister)
+
 
 module.exports = router;
