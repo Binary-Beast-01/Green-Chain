9331c4b5a22c3895eb3477e1c2c7a36feba4fd76
MUKILAN S <mukilan069@gmail.com>
2023-02-19 00:32:33 +0530

    Dynamic Dashboard Init

--- app.js
+++ app.js
@@ -14,12 +14,15 @@ app.use(cookieParser())
 
 //Routers
 const AuthenticationRoute = require('./routes/authRoutes.js')
+const DashboardRoute = require('./routes/dashboardRoute')
+
 
 const PORT = process.env.PORT || 8888
 app.listen(PORT, () => { console.log('Server Check.................OK !!\nhttp://localhost:8888') });
 
 // Routering operations
 app.use('/auth', AuthenticationRoute)
+app.use('/',DashboardRoute)
 
 app.get('/',(req,res)=>{
   res.render("index")
