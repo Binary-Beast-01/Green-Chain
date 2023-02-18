f2ad6f750e783e0fc8bf917a83f267a1aafd7f0b
K.Tensh Kumar <tenshkumarkkt@gmail.com>
2023-02-18 15:48:25 +0530

    init node js template

---
+++ app.js
@@ -0,0 +1,20 @@
+require('dotenv').config()
+const express = require("express")
+const cookieParser = require("cookie-parser")
+const app = express()
+
+app.set('view engine', 'ejs')
+
+app.use(express.static('public'))
+app.use(express.json())
+app.use(express.urlencoded({
+  extended: true
+}))
+app.use(cookieParser())
+
+const PORT = process.env.PORT || 8888
+app.listen(PORT, () => { console.log('API Server.................ok') });
+
+app.get('/',(req,res)=>{
+  res.send("Under Development.......")
+})
